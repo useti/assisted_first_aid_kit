58860b43dcd80f0d4a66aebf43c35e58bcd7e968
Yury Tikhoglaz <usetiskip@me.com>
2017-04-25 23:29:06 +0300

    First approach
    Embeded DTMF decoder - Enable AT+DDET=1
    Call number - ATD+7XXXXXXXXXX;

--- SOFTWARE/Controller/controller/controller.ino
+++ SOFTWARE/Controller/controller/controller.ino
@@ -1,26 +1,62 @@
-#include <SoftwareSerial.h>
+// библиотека для работы с GPRS устройством
 #include <GPRS_Shield_Arduino.h>
-
+ 
+// создаём объект класса GPRS и передаём в него объект Serial1 
 GPRS gprs(Serial1);
-
-void setup() {
-  gprs.powerOn();
+// можно указать дополнительные параметры — пины PK и ST
+// по умолчанию: PK = 2, ST = 3
+// GPRS gprs(Serial1, 2, 3);
+ 
+void setup()
+{ 
+  // открываем последовательный порт для мониторинга действий в программе
+  Serial.begin(9600);
   
-  Serial.begin(9600); 
-  while (!Serial) {
-    
-  }
+  Serial.print("Serial init OK\r\n");
+  // открываем Serial-соединение с GPRS Shield
+  Serial1.begin(9600);
+  // включаем GPRS шилд
+  gprs.powerOn();
+  // проверяем есть ли связь с GPRS устройством
   while (!gprs.init()) {
-    
-    delay(1000);
-    Serial.print("Init error\r\n");
+    // если связи нет, ждём 1 секунду
+    // и выводим сообщение об ошибке
+    // процесс повторяется в цикле
+    // пока не появится ответ от GPRS устройства
+    Serial.print("GPRS Init error\r\n");
+    delay(3000);
   }
-  //gprs.sendSMS("+79263995140", "Hello SMS from Amperka!");
+  // выводим сообщение об удачной инициализации GPRS Shield
+  Serial.println("GPRS init success");
 
-  gprs.powerOff();
+  Serial1.println("AT+DDET=1");
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+ 
+void loop()
+{
+  serialPCread();
+  // считываем данные с GPRS Shield и выводим их в Serial-порт
+  serialGPRSread();
+}
+ 
+void serialPCread()
+{
+  if (Serial.available() > 0) {
+    // если приходят данные по USB
+    while (Serial.available() > 0) {
+      // записываем их в GPRS Shield
+      Serial1.write(Serial.read());
+    }
+  }
+}
+ 
+void serialGPRSread()
+{
+    if (Serial1.available() > 0) {
+      // если приходят данные с GPRS Shield
+      while (Serial1.available() > 0) {
+        // передаём их в USB
+        Serial.write(Serial1.read());
+    }
+  }
 }
